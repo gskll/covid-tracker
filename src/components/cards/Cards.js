45bfa5d4aa3b9accc7a58ded7d4dc54459c023fd
Andrew <gskll@protonmail.com>
2020-12-13 22:38:33 +0100

    feat: add api total data to Card component

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
 
 import styles from "./Cards.module.css";
 
-const Cards = (props) => {
-  console.log(props.data);
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return "Loading...";
+  }
 
   return (
-    <div class={styles.container}>
+    <div className={styles.container}>
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <CardContent>
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">Real data</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator=","
+              ></CountUp>
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
             </Typography>
@@ -26,8 +38,17 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">Real data</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={recovered.value}
+                duration={2.5}
+                separator=","
+              ></CountUp>
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of recoveries from COVID-19
             </Typography>
@@ -38,8 +59,17 @@ const Cards = (props) => {
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">Real data</Typography>
-            <Typography color="textSecondary">Real Date</Typography>
+            <Typography variant="h5">
+              <CountUp
+                start={0}
+                end={deaths.value}
+                duration={2.5}
+                separator=","
+              ></CountUp>
+            </Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
             <Typography variant="body2">
               Number of deaths caused by COVID-19
             </Typography>
