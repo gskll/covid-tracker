20c0ebf00f2561399909604252376e3bc559d111
Andrew <gskll@protonmail.com>
2020-12-13 12:08:45 +0100

    feat: fetch totals data and pass to Cards comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./";
+import { fetchData } from "../api";
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  state = { data: {} };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <CountryPicker />
         <Chart />
       </div>
